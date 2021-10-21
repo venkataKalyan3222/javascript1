52e9f7e7b78cf583b0bee5f92abc03b0a3dadaf0
venkataKalyan3222 <venkataKalyan3222@gmail.com>
2021-10-21 11:08:01 +0530

    ok

---
+++ five1.js
@@ -0,0 +1,3 @@
+//Write  a program to print 1-10 even numbers using while loop // 
+var i=10;
+while(i%2=0)
---
+++ if/ten.js
@@ -0,0 +1,16 @@
+//Write  a program to check a Least number in given three numbers// 
+var x=10;
+var y=32;
+var z=12;
+if(x<y && x<z){
+    console.log("x is a Least number among three")
+}
+
+else if(y<z){
+    console.log("y is a Least number among three")
+}
+else
+{
+    console.log("z is a Least number among three")
+    
+}
