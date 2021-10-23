65baf23e235f79136c082830513b4e526d9494dd
venkataKalyan3222 <venkataKalyan3222@gmail.com>
2021-10-23 11:59:48 +0530

    kalyan

---
+++ if/Greatest no.js
@@ -0,0 +1,14 @@
+//Write  a program to check a gratest number in given three numbers// 
+var x=234;
+var y=26;
+var z=12;
+if(x>y && x>z){
+    console.log("x is a gratest number among three")
+}
+else if(y>z && y>x){
+    console.log("y is a gratest number among three")
+}
+    else 
+{
+    console.log("z is a gratest number among three")
+}
